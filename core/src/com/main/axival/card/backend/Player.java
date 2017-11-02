46a0707e546ac9bca7c9c962a218e22db9470fb8
JoesSattes <59070173@it.kmitl.ac.th>
2017-11-02 23:37:40 +0700

    insert player status

---
+++ core/src/com/main/axival/card/backend/Player.java
@@ -0,0 +1,17 @@
+package com.main.axival.card.backend;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player {
+    private Map status;
+    public Player(){
+        this.status = new HashMap();
+    }
+
+    public void setStatus(String keyMap, String valueMap){
+        if (valueMap.equals("true")){
+            status[keyMap] = true;
+        }
+    }
+}

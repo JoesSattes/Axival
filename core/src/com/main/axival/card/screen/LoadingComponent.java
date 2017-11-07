3255df8d9eb7b999c06aafa69cf1dd924dd434b2
JoesSattes <59070173@it.kmitl.ac.th>
2017-11-07 19:10:32 +0700

    add new style loading

--- core/src/com/main/axival/card/screen/LoadingComponent.java
+++ core/src/com/main/axival/card/screen/LoadingComponent.java
@@ -5,12 +5,18 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.main.axival.card.CardPlay;
 import com.main.axival.card.Menu;
 
@@ -19,6 +25,19 @@ public class LoadingComponent implements Screen{
     private final CardPlay cardPlay;
     private ShapeRenderer shapeRenderer;
     private float progress;
+
+    //add new loading screen
+    private Stage stage;
+    private TextureAtlas textureAtPack;
+    private Image logo;
+    private Image loadingFrame;
+    private Image loadingBarHidden;
+    private Image screenBg;
+    private Image loadingBg;
+    private Animation anim;
+    private Actor loadingBar;
+    private float startX, endX;
+    private float percent;
 
 
     public LoadingComponent(final CardPlay cardPlay){
@@ -32,6 +51,24 @@ public class LoadingComponent implements Screen{
     @Override
     public void show() {
         System.out.println("Loading");
+        //add new loading screen
+        stage = new Stage(new FillViewport(CardPlay.V_WIDTH,CardPlay.V_HEIGHT));
+        textureAtPack = cardPlay.assetManager.get("load/loading.pack", TextureAtlas.class);
+        logo = new Image(textureAtPack.findRegion("libgdx-logo"));
+        loadingFrame = new Image(textureAtPack.findRegion("loading-frame"));
+        loadingBarHidden = new Image(textureAtPack.findRegion("loading-bar-hidden"));
+        screenBg = new Image(textureAtPack.findRegion("screen-bg"));
+        loadingBg = new Image(textureAtPack.findRegion("loading-frame-bg"));
+        anim = new Animation(0.05f, textureAtPack.findRegions("loading-bar-anim"));
+        loadingBar = new LoadingBar(anim);
+        loadingBar = new Image(textureAtPack.findRegion("loading-bar1"));
+        stage.addActor(screenBg);
+        stage.addActor(loadingBar);
+        stage.addActor(loadingBg);
+        stage.addActor(loadingBarHidden);
+        stage.addActor(loadingFrame);
+        stage.addActor(logo);
+
     }
 
     private void update(float delta){
@@ -60,11 +97,52 @@ public class LoadingComponent implements Screen{
         cardPlay.batch.begin();
         cardPlay.bitmapFont.draw(cardPlay.batch, "Screen : Loading", 20, 20);
         cardPlay.batch.end();
+
+        //add new loading screen
+        percent = Interpolation.linear.apply(percent, cardPlay.assetManager.getProgress(), 0.1f);
+        loadingBarHidden.setX(startX + endX * percent);
+        loadingBg.setX(loadingBarHidden.getX() + 30);
+        loadingBg.setWidth(450 - 450 * percent);
+        loadingBg.invalidate();
+        // Show the loading screen
+        stage.act();
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+        //add new loading screen
+        width = 1280;
+        height = 720;
+        //stage.setViewport(width, height, false);
+        stage.getViewport().update(width,height);
 
+        // Make the background fill the screen
+        screenBg.setSize(width, height);
+
+        // Place the logo in the middle of the screen and 100 px up
+        logo.setX((width - logo.getWidth()) / 2);
+        logo.setY((height - logo.getHeight()) / 2 + 100);
+
+        // Place the loading frame in the middle of the screen
+        loadingFrame.setX((stage.getWidth() - loadingFrame.getWidth()) / 2);
+        loadingFrame.setY((stage.getHeight() - loadingFrame.getHeight()) / 2);
+
+        // Place the loading bar at the same spot as the frame, adjusted a few px
+        loadingBar.setX(loadingFrame.getX() + 15);
+        loadingBar.setY(loadingFrame.getY() + 5);
+
+        // Place the image that will hide the bar on top of the bar, adjusted a few px
+        loadingBarHidden.setX(loadingBar.getX() + 35);
+        loadingBarHidden.setY(loadingBar.getY() - 3);
+        // The start position and how far to move the hidden loading bar
+        startX = loadingBarHidden.getX();
+        endX = 440;
+
+        // The rest of the hidden bar
+        loadingBg.setSize(450, 50);
+        loadingBg.setX(loadingBarHidden.getX() + 30);
+        loadingBg.setY(loadingBarHidden.getY() + 3);
     }
 
     @Override
@@ -95,5 +173,9 @@ public class LoadingComponent implements Screen{
         cardPlay.assetManager.load("cursorImage2.png", Texture.class);
         cardPlay.assetManager.load("bg1.jpg", Texture.class);
         cardPlay.assetManager.load("effect01.party", ParticleEffect.class);
+
+        //add new loading screen
+        cardPlay.assetManager.load("load/loading.pack", TextureAtlas.class);
+        cardPlay.assetManager.finishLoading();
     }
 }

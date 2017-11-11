bc24bca393bd13d1da8db9f8d82f89cd0f386621
panudet40 <59070130@it.kmitl.ac.th>
2017-11-11 19:10:17 +0700

    test commit from new branch

--- desktop/src/com/testgdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/testgdx/game/desktop/DesktopLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.testgdx.game.TestGdx;
 
-public class DesktopLauncher {
+public class DesktopLauncher {//kuy
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
